98b58df776ffdd019f73cd5fcb1acc12e5b33a6a
0x3lden <153280243+0x3lden@users.noreply.github.com>
2024-05-21 17:08:58 +0200

    Simplify

--- src/routes/overview/Overview.tsx
+++ src/routes/overview/Overview.tsx
@@ -1,23 +1,14 @@
 import { Grid } from '@mui/material';
-import { useContext, useState } from 'react';
+import { useContext } from 'react';
 import { OverviewTable } from '../../components/OverviewTable';
-import { SimpleAlert } from '../../components/SimpleAlert';
 import { AppContext } from '../App';
 
 export function Overview() {
-  const [openAlert, setOpenAlert] = useState(false);
-  const [alertMsg] = useState('');
   const { contextVariables } = useContext(AppContext);
-
-  const handleCloseAlert = () => {
-    setOpenAlert(false);
-  };
 
   return (
     <Grid sx={{ mt: 10 }} container spacing={2}>
       <OverviewTable data={contextVariables.overviewData} />
-
-      <SimpleAlert alertMsg={alertMsg} handleCloseAlert={handleCloseAlert} openAlert={openAlert} />
     </Grid>
   );
 }
